408a8a95dfe96ccbb8d3a8b18a8ce0ec95a0fc84
The5-1 <agaku@hotmail.de>
2019-03-16 19:38:27 +0100

    scope timer

--- ScopeTimer.hxx
+++ ScopeTimer.hxx
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <chrono>
+#include <cstdint>
 #include <iostream>
 
 namespace the5 {
@@ -9,7 +10,7 @@ namespace util {
 
 	class ScopeTimer {
 	public:
-		ScopeTimer(const char* text = "", int line = -1, unsigned int iterations = 0) :text(text), line(line), iterations(iterations)
+		ScopeTimer(const char* text = "", size_t iterations = 0, unsigned int line = 0) :text(text), line(line), iterations(iterations)
 		{
 			start = std::chrono::high_resolution_clock::now();
 		}
@@ -20,24 +21,23 @@ namespace util {
 			auto dur = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 			long long int microsec = static_cast<long long int>(dur.count());
 			if (iterations > 2) microsec /= iterations;
-
 			long double millisec = microsec / 1000.0;
 
 			printf("Timer: %s ", text);
-			if (line > -1) printf("in line %u", line);
-			if (iterations > 2) printf("(avg. for %u iterations) ", iterations);
+			if (line > 0) printf("in line %u ", line);
+			if (iterations > 1) printf("(avg. for %u iterations) ", iterations);
 			printf("took %Lfms\n", millisec);
 		}
 
 	protected:
-		unsigned int iterations;
+		size_t iterations;
 		const char* text;
 		unsigned int line;
 		std::chrono::time_point<std::chrono::high_resolution_clock> start;
 	};
 
-#define SCOPETIMER() the5::util::ScopeTimer t(__FILE__, __LINE__, 0)
-#define SCOPETIMERAVG(iterations) the5::util::ScopeTimer t(__FILE__, __LINE__,iterations)
+#define SCOPETIMER() the5::util::ScopeTimer t(__FILE__, 0, __LINE__)
+#define SCOPETIMERAVG(iterations) the5::util::ScopeTimer t(__FILE__, iterations, __LINE__)
 
 }
 }
